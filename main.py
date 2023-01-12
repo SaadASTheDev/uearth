a0311126661c4fb5dea92f6c62833998ca4b74bd
Saad Ahmad Sabri <105510022+SaadASTheDev@users.noreply.github.com>
2023-01-12 14:11:57 -0600

    Import Files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Import Nescessary Files
+import torch as pt
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,4 +7,5 @@ import seaborn as sns
 import os
 import random as rn
 
-# Model Based Python Machine Learning Program
+#Importing the Dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
