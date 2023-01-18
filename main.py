99ba8462046b2248565e95ff9f2a1e167a24b926
Saad Ahmad Sabri <105510022+SaadASTheDev@users.noreply.github.com>
2023-01-18 10:48:44 -0600

    Import Packages

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-#Import Nescessary Files
-import torch as pt
+# Import Necessary Files
+import py as pt
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 import random as rn
+from sklearn.ensemble import RandomForestRegressor
 
-#Importing the Dataset
-dataset = pd.read_csv('Churn_Modelling.csv')
+# Importing the Dataset
+dataset = pd.read_csv('.csv')
