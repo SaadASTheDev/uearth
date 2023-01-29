829d8aa1249b48fa60c87fe71033cfcfab30a00f
Saad Ahmad Sabri <105510022+SaadASTheDev@users.noreply.github.com>
2023-01-28 19:34:59 -0500

    Small Preference changes

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-import pandas as pd
-import random
-import numpy as np
-from sklearn.ensemble import RandomForestRegressor
-import folium
+# Import libraries
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns # for data visualization
+import matplotlib.pyplot as plt # to plot charts
+from collections import Counter
+import os
+
+# Modeling Libraries
+from sklearn.preprocessing import QuantileTransformer
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFold, learning_curve, train_test_split
 
